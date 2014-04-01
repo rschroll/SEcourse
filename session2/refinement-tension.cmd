fb0db355b20fb5e528b2ead9fc79761e5d68bbea
Robert Schroll <rschroll@gmail.com>
2014-04-01 12:58:58 -0400

    Session 2

---
+++ session2/refinement-tension.cmd
@@ -0,0 +1,54 @@
+
+// Set form factors so reference shape is unstretched.
+set_form_factors := {
+  foreach facet ff do
+  { set ff.form_factors[1]
+          (ff.vertex[2].ref_coord[1] - ff.vertex[1].ref_coord[1])^2
+        + (ff.vertex[2].ref_coord[2] - ff.vertex[1].ref_coord[2])^2
+        + (ff.vertex[2].ref_coord[3] - ff.vertex[1].ref_coord[3])^2;
+    set ff.form_factors[2]
+          (ff.vertex[2].ref_coord[1] - ff.vertex[1].ref_coord[1])
+         *(ff.vertex[3].ref_coord[1] - ff.vertex[1].ref_coord[1])
+        + (ff.vertex[2].ref_coord[2] - ff.vertex[1].ref_coord[2])
+         *(ff.vertex[3].ref_coord[2] - ff.vertex[1].ref_coord[2])
+        + (ff.vertex[2].ref_coord[3] - ff.vertex[1].ref_coord[3])
+         *(ff.vertex[3].ref_coord[3] - ff.vertex[1].ref_coord[3]);
+    set ff.form_factors[3]
+          (ff.vertex[3].ref_coord[1] - ff.vertex[1].ref_coord[1])^2
+        + (ff.vertex[3].ref_coord[2] - ff.vertex[1].ref_coord[2])^2
+        + (ff.vertex[3].ref_coord[3] - ff.vertex[1].ref_coord[3])^2;
+  }
+}
+set_form_factors;
+
+// redefine the "r" command to adjust form factors.
+r :::= {
+    set vertex old_vid id;
+    set edge old_eid id;
+
+    minlen := 1.366*min(edges, length);
+    // Lengths should be near min, sqrt(3)*min, and 2*min; want to catch
+    // these last two cohorts.
+    refine edges where length > minlen;
+    u;
+
+    foreach vertex vv where old_vid == 0 do {
+        vv.ref_coord[1] :=
+            avg(vv.edge ee where old_eid != 0,sum(ee.vertex where old_vid != 0, ref_coord[1]));
+        vv.ref_coord[2] :=
+            avg(vv.edge ee where old_eid != 0,sum(ee.vertex where old_vid != 0, ref_coord[2]));
+        vv.ref_coord[3] :=
+            avg(vv.edge ee where old_eid != 0,sum(ee.vertex where old_vid != 0, ref_coord[3]));
+    };
+    set_form_factors;
+
+    foreach vertex vv do {
+        if max(vv.edge, tension_mark) > 0 then {
+            set vv apptension;
+            vv.tension_length := sum(vv.edge ee where tension_mark,
+                    abs(ee.vertex[1].ref_coord[2] - ee.vertex[2].ref_coord[2])/2)
+        };
+    };
+
+    recalc;
+}
